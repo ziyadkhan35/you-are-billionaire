4fc1bb93ea61bdb3ae63f2494c59db394413adea
murudovz17 <murudovz17@gmail.com>
2022-09-10 17:38:02 +0400

    url6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <ToastContainer/>
       <Header/>
       <Routes>
-        <Route path="/you-are-billionaire" element={<Home />} />
+        <Route path="#" element={<Home />} />
         <Route path="basket" element={<Basket />} />
         <Route path="about-us" element={<About />} />
       </Routes>
